fb610926c6f570caad5d38aac8737d05aebbe0db
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-18 05:09:08 +0300

    Add footer

---
+++ src/react-components/footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style.css"
+
+function Footer() {
+  return (
+    <footer className="footer">
+      All rights reserved 2022
+    </footer>
+  )
+}
+
+export default Footer
