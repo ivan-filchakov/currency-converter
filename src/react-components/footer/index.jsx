6cfa94cbcd49ed266cb26b8ca484f0e34676ee21
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-20 04:37:34 +0300

    Footer info update

--- src/react-components/footer/index.jsx
+++ src/react-components/footer/index.jsx
@@ -1,10 +1,23 @@
 import React from "react"
+import useRatesData from "../../store/store.selectors"
+import apiURL from "../../api/api.url"
 import "./style.css"
 
 function Footer() {
+  const { timestamp } = useRatesData()
+  const directLink = apiURL.split("api.").join("")
   return (
     <footer className="footer">
-      All rights reserved 2022
+      <span>
+        Exchange rates data from:&nbsp;
+        <a href={directLink} target="_blank" rel="noopener noreferrer">
+          {directLink}
+        </a>
+      </span>
+      <span>
+        Timestamp:&nbsp;
+        {timestamp}
+      </span>
     </footer>
   )
 }
