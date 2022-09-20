e93bf39181c4350257825cab8585306c1b952125
ivan-filchakov <coolstorybrotellitagainpls@gmail.com>
2022-09-20 07:26:50 +0300

    Footer update

--- src/react-components/footer/index.jsx
+++ src/react-components/footer/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { BsGithub } from "react-icons/bs"
 import useRatesData from "../../store/store.selectors"
 import apiURL from "../../api/api.url"
 import "./style.css"
@@ -8,16 +9,27 @@ function Footer() {
   const directLink = apiURL.split("api.").join("")
   return (
     <footer className="footer">
-      <span>
-        Exchange rates data from:&nbsp;
-        <a href={directLink} target="_blank" rel="noopener noreferrer">
-          {directLink}
+      <div className="footer__info">
+        <span>
+          Exchange rates data from:&nbsp;
+          <a href={directLink} target="_blank" rel="noopener noreferrer">
+            {directLink}
+          </a>
+        </span>
+        <span>
+          Timestamp:&nbsp;
+          {timestamp}
+        </span>
+      </div>
+      <div className="footer__button">
+        <a
+          href="https://github.com/ivan-filchakov"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsGithub size="32px" color="#2c2c2c" />
         </a>
-      </span>
-      <span>
-        Timestamp:&nbsp;
-        {timestamp}
-      </span>
+      </div>
     </footer>
   )
 }
